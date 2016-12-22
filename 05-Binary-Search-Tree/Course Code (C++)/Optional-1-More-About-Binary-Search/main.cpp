4dad16b0b5f4f1e7ec998e2d743a4c621b65a6bb
liuyubobobo <liuyubobobo@gmail.com>
2016-12-22 22:42:57 +0800

    Chapter 05 Optional 01 Advanced Binary Search code added.

---
+++ 05-Binary-Search-Tree/Course Code (C++)/Optional-1-More-About-Binary-Search/main.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <cassert>
+#include <ctime>
+
+using namespace std;
+
+// 二分查找法, 在有序数组arr中, 查找target
+// 如果找到target, 返回第一个target相应的索引index
+// 如果没有找到target, 返回比target小的最大值相应的索引, 如果这个最大值有多个, 返回最大索引
+// 如果这个target比整个数组的最小元素值还要小, 则不存在这个target的floor值, 返回-1
+template<typename T>
+int floor(T arr[], int n, T target){
+
+    assert( n >= 0 );
+
+    // 寻找比target小的最大索引
+    int l = -1, r = n-1;
+    while( l < r ){
+        // 使用向上取整避免死循环
+        int mid = l + (r-l+1)/2;
+        if( arr[mid] >= target )
+            r = mid - 1;
+        else
+            l = mid;
+    }
+
+    assert( l == r );
+
+    // 如果该索引+1就是target本身, 该索引+1即为返回值
+    if( l + 1 < n && arr[l+1] == target )
+        return l + 1;
+
+    // 否则, 该索引即为返回值
+    return l;
+}
+
+
+// 二分查找法, 在有序数组arr中, 查找target
+// 如果找到target, 返回最后一个target相应的索引index
+// 如果没有找到target, 返回比target大的最小值相应的索引, 如果这个最小值有多个, 返回最小的索引
+// 如果这个target比整个数组的最大元素值还要大, 则不存在这个target的ceil值, 返回整个数组元素个数n
+template<typename T>
+int ceil(T arr[], int n, T target){
+
+    assert( n >= 0 );
+
+    // 寻找比target大的最小索引值
+    int l = 0, r = n;
+    while( l < r ){
+        // 使用普通的向下取整即可避免死循环
+        int mid = l + (r-l)/2;
+        if( arr[mid] <= target )
+            l = mid + 1;
+        else // arr[mid] > target
+            r = mid;
+    }
+
+    assert( l == r );
+
+    // 如果该索引-1就是target本身, 该索引+1即为返回值
+    if( r - 1 >= 0 && arr[r-1] == target )
+        return r-1;
+
+    // 否则, 该索引即为返回值
+    return r;
+}
+
+
+int main(){
+
+    int a[] = {1, 1, 1, 2, 2, 2, 2, 2, 4, 4, 5, 5, 5, 6, 6, 6};
+    int n = sizeof(a)/sizeof(int);
+    for( int i = 0 ; i <= 8 ; i ++ ){
+
+        int floorIndex = floor(a, n, i);
+        cout<<"the floor index of "<<i<<" is "<<floorIndex<<".";
+        if( floorIndex >= 0 && floorIndex < n )
+            cout<<"The value is "<<a[floorIndex]<<".";
+        cout<<endl;
+
+        int ceilIndex = ceil(a, sizeof(a)/sizeof(int), i);
+        cout<<"the ceil index of "<<i<<" is "<<ceilIndex<<".";
+        if( ceilIndex >= 0 && ceilIndex < n )
+            cout<<"The value is "<<a[ceilIndex]<<".";
+        cout<<endl;
+
+        cout<<endl;
+    }
+
+    return 0;
+}
