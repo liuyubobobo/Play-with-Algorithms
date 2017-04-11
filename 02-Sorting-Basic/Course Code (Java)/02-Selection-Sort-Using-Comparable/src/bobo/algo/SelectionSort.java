c185487b1b2dfb84e710081505014e5a24f1cbca
liuyubobobo <liuyubobobo@gmail.com>
2017-04-10 23:35:15 -0700

    02-01 java code updated and 02-02 java code added.

--- 02-Sorting-Basic/Course Code (Java)/02-Selection-Sort-Using-Comparable/src/bobo/algo/SelectionSort.java
+++ 02-Sorting-Basic/Course Code (Java)/02-Selection-Sort-Using-Comparable/src/bobo/algo/SelectionSort.java
@@ -5,29 +5,30 @@ public class SelectionSort {
     // 我们的算法类不允许产生任何实例
     private SelectionSort(){}
 
-    public static void sort(int[] arr){
+    public static void sort(Comparable[] arr){
 
         int n = arr.length;
         for( int i = 0 ; i < n ; i ++ ){
             // 寻找[i, n)区间里的最小值的索引
             int minIndex = i;
             for( int j = i + 1 ; j < n ; j ++ )
-                if( arr[j] < arr[minIndex] )
+                // 使用compareTo方法比较两个Comparable对象的大小
+                if( arr[j].compareTo( arr[minIndex] ) < 0 )
                     minIndex = j;
 
             swap( arr , i , minIndex);
         }
     }
 
-    private static void swap(int[] arr, int i, int j) {
-        int t = arr[i];
+    private static void swap(Object[] arr, int i, int j) {
+        Object t = arr[i];
         arr[i] = arr[j];
         arr[j] = t;
     }
 
     public static void main(String[] args) {
 
-        int[] arr = {10,9,8,7,6,5,4,3,2,1};
+        Integer[] arr = {10,9,8,7,6,5,4,3,2,1};
         SelectionSort.sort(arr);
         for( int i = 0 ; i < arr.length ; i ++ ){
             System.out.print(arr[i]);
