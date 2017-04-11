af688a55e2cbd49cad9b9afec70093c5eeff9409
liuyubobobo <liuyubobobo@gmail.com>
2017-04-10 22:40:14 -0700

    02-01 java code added.

---
+++ 02-Sorting-Basic/Course Code (Java)/01-Selection-Sort/src/com/liuyubobobo/SelectionSort.java
@@ -0,0 +1,41 @@
+package com.liuyubobobo;
+
+public class SelectionSort {
+
+    // 我们的算法类不允许产生任何实例
+    private SelectionSort(){}
+
+    public static void sort(Comparable[] arr){
+
+        int n = arr.length;
+        for( int i = 0 ; i < n ; i ++ ){
+            // 寻找[i, n)区间里的最小值的索引
+            int minIndex = i;
+            for( int j = i + 1 ; j < n ; j ++ )
+                // 使用compareTo方法比较两个Comparable对象的大小
+                if( arr[j].compareTo( arr[minIndex] ) < 0 )
+                    minIndex = j;
+
+            swap( arr , i , minIndex);
+        }
+    }
+
+    private static void swap(Object[] arr, int i, int j) {
+        Object t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+
+    public static void main(String[] args) {
+
+        Integer[] arr = {10,9,8,7,6,5,4,3,2,1};
+        SelectionSort.sort(arr);
+        for( int i = 0 ; i < arr.length ; i ++ ){
+            System.out.print(arr[i]);
+            System.out.print(' ');
+        }
+        System.out.println();
+    }
+}
+
+
