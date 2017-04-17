c60940570dfcac795ca732e241d4e707b02796fa
liuyubobobo <liuyubobobo@gmail.com>
2017-04-17 16:22:26 -0700

    Chapter 02 section 03 java code updated.

---
+++ 02-Sorting-Basic/Course Code (Java)/03-Selection-Sort-Generate-Test-Cases/src/bobo/algo/SortTestHelper.java
@@ -0,0 +1,31 @@
+package bobo.algo;
+
+public class SortTestHelper {
+
+    // SortTestHelper不允许产生任何实例
+    private SortTestHelper(){}
+
+    // 生成有n个元素的随机数组,每个元素的随机范围为[rangeL, rangeR]
+    static public Integer[] generateRandomArray(int n, int rangeL, int rangeR) {
+
+        assert rangeL <= rangeR;
+
+        Integer[] arr = new Integer[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = new Integer((int)(Math.random() * (rangeR - rangeL + 1) + rangeL));
+        return arr;
+    }
+
+    // 打印arr数组的所有内容
+    static void printArray(Object arr[]) {
+
+        for (int i = 0; i < arr.length; i++){
+            System.out.print( arr[i] );
+            System.out.print( ' ' );
+        }
+        System.out.println();
+
+        return;
+    }
+}
