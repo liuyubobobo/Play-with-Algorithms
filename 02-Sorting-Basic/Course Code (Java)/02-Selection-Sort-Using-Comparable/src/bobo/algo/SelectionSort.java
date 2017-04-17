49adbcd7d298bc685f5af7ea51abcefbb8d7bd42
liuyubobobo <liuyubobobo@gmail.com>
2017-04-17 12:04:54 -0700

    Chapter 02 section 02 java code updated.

--- 02-Sorting-Basic/Course Code (Java)/02-Selection-Sort-Using-Comparable/src/bobo/algo/SelectionSort.java
+++ 02-Sorting-Basic/Course Code (Java)/02-Selection-Sort-Using-Comparable/src/bobo/algo/SelectionSort.java
@@ -1,5 +1,7 @@
 package bobo.algo;
 
+import java.util.*;
+
 public class SelectionSort {
 
     // 我们的算法类不允许产生任何实例
@@ -28,12 +30,41 @@ public class SelectionSort {
 
     public static void main(String[] args) {
 
-        Integer[] arr = {10,9,8,7,6,5,4,3,2,1};
-        SelectionSort.sort(arr);
-        for( int i = 0 ; i < arr.length ; i ++ ){
-            System.out.print(arr[i]);
+        // 测试Integer
+        Integer[] a = {10,9,8,7,6,5,4,3,2,1};
+        SelectionSort.sort(a);
+        for( int i = 0 ; i < a.length ; i ++ ){
+            System.out.print(a[i]);
             System.out.print(' ');
         }
         System.out.println();
+
+        // 测试Double
+        Double[] b = {4.4, 3.3, 2.2, 1.1};
+        SelectionSort.sort(b);
+        for( int i = 0 ; i < b.length ; i ++ ){
+            System.out.print(b[i]);
+            System.out.print(' ');
+        }
+        System.out.println();
+
+        // 测试String
+        String[] c = {"D", "C", "B", "A"};
+        SelectionSort.sort(c);
+        for( int i = 0 ; i < c.length ; i ++ ){
+            System.out.print(c[i]);
+            System.out.print(' ');
+        }
+        System.out.println();
+
+        // 测试自定义的类 Student
+        Student[] d = new Student[4];
+        d[0] = new Student("D",90);
+        d[1] = new Student("C",100);
+        d[2] = new Student("B",95);
+        d[3] = new Student("A",95);
+        SelectionSort.sort(d);
+        for( int i = 0 ; i < d.length ; i ++ )
+            System.out.println(d[i]);
     }
 }
---
+++ 02-Sorting-Basic/Course Code (Java)/02-Selection-Sort-Using-Comparable/src/bobo/algo/Student.java
@@ -0,0 +1,36 @@
+package bobo.algo;
+
+import java.util.*;
+
+public class Student implements Comparable<Student> {
+
+    private String name;
+    private int score;
+
+    public Student(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+
+    // 定义Student的compareTo函数
+    // 如果分数相等，则按照名字的字母序排序
+    // 如果分数不等，则分数高的靠前
+    @Override
+    public int compareTo(Student that) {
+
+        if( this.score == that.score )
+            return this.name.compareTo(that.name);
+
+        if( this.score < that.score )
+            return 1;
+        else if( this.score > that.score )
+            return -1;
+        else // this.score == that.score
+            return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Student: " + this.name + " " + Integer.toString( this.score );
+    }
+}
