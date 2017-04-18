5cf629ad033312fb2ca6a98dbb10d2ca81ec749b
liuyubobobo <liuyubobobo@gmail.com>
2017-04-18 12:41:55 -0700

    Chapter 02 section 04 java code added. C++ code comments updated.

--- 02-Sorting-Basic/Course Code (C++)/04-Selection-Sort-Detect-Performance/SortTestHelper.h
+++ 02-Sorting-Basic/Course Code (C++)/04-Selection-Sort-Detect-Performance/SortTestHelper.h
@@ -29,6 +29,7 @@ namespace SortTestHelper {
         return arr;
     }
 
+    // 打印arr数组的所有内容
     template<typename T>
     void printArray(T arr[], int n) {
 
@@ -39,6 +40,7 @@ namespace SortTestHelper {
         return;
     }
 
+    // 判断arr数组是否有序
     template<typename T>
     bool isSorted(T arr[], int n) {
 
@@ -49,6 +51,14 @@ namespace SortTestHelper {
         return true;
     }
 
+    // 测试sort排序算法排序arr数组所得到结果的正确性和算法运行时间
+    // * 使用VS编码的同学, 对于函数指针的写法和调用方法可能和课程中介绍的有所不同;
+    // * 并且不同版本的VS, 其具体语法可能也有差异, 这是因为VS的编译器不完全是按照C++的标准实现的;
+    // * 本课程按照C++11的标准进行书写。对于VS编译器带来的语法差异, 希望同学们可以自己在网上查找相关资料解决;
+    // * 大家也可以在课程的官方QQ群中交流
+    // * 另外, 使用函数指针本身并不是这个课程的重点, 大家也完全可以使用自己的方式书写代码, 最终只要能够测试出自己书写的算法的效率即可
+    // * 推荐大家阅读我在问答区向大家分享的一个学习心得: 【学习心得分享】请大家抓大放小，不要纠结于C++语言的语法细节
+    // * 链接: http://coding.imooc.com/learn/questiondetail/4100.html
     template<typename T>
     void testSort(const string &sortName, void (*sort)(T[], int), T arr[], int n) {
 
---
+++ 02-Sorting-Basic/Course Code (Java)/04-Selection-Sort-Detect-Performance/src/bobo/algo/SortTestHelper.java
@@ -0,0 +1,72 @@
+package bobo.algo;
+
+import java.lang.reflect.Method;
+import java.lang.Class;
+
+public class SortTestHelper {
+
+    // SortTestHelper不允许产生任何实例
+    private SortTestHelper(){}
+
+    // 生成有n个元素的随机数组,每个元素的随机范围为[rangeL, rangeR]
+    static public Integer[] generateRandomArray(int n, int rangeL, int rangeR) {
+
+        assert rangeL <= rangeR;
+
+        Integer[] arr = new Integer[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = new Integer((int)(Math.random() * (rangeR - rangeL + 1) + rangeL));
+        return arr;
+    }
+
+    // 打印arr数组的所有内容
+    static void printArray(Object arr[]) {
+
+        for (int i = 0; i < arr.length; i++){
+            System.out.print( arr[i] );
+            System.out.print( ' ' );
+        }
+        System.out.println();
+
+        return;
+    }
+
+    // 判断arr数组是否有序
+    static boolean isSorted(Comparable[] arr){
+
+        for( int i = 0 ; i < arr.length - 1 ; i ++ )
+            if( arr[i].compareTo(arr[i+1]) > 0 )
+                return false;
+        return true;
+    }
+
+    // 测试sortClassName所对应的排序算法排序arr数组所得到结果的正确性和算法运行时间
+    static void testSort(String sortClassName, Comparable[] arr){
+
+        // 通过Java的反射机制，通过排序的类名，运行排序函数
+        // * 依然是，使用反射机制并不是这个课程的重点, 大家也完全可以使用自己的方式书写代码, 最终只要能够测试出自己书写的算法的效率即可
+        // * 推荐大家阅读我在问答区向大家分享的一个学习心得: 【学习心得分享】请大家抓大放小，不要纠结于C++语言的语法细节
+        // * 链接: http://coding.imooc.com/learn/questiondetail/4100.html
+        try{
+            // 通过sortClassName获得排序函数的Class对象
+            Class sortClass = Class.forName(sortClassName);
+            // 通过排序函数的Class对象获得排序方法
+            Method sortMethod = sortClass.getMethod("sort",new Class[]{Comparable[].class});
+            // 排序参数只有一个，是可比较数组arr
+            Object[] params = new Object[]{arr};
+
+            long startTime = System.currentTimeMillis();
+            // 调用排序函数
+            sortMethod.invoke(null,params);
+            long endTime = System.currentTimeMillis();
+
+            assert isSorted( arr );
+
+            System.out.println( sortClass.getSimpleName()+ " : " + (endTime-startTime) + "ms" );
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
