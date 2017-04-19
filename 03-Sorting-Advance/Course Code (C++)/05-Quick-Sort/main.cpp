8fae4b58916e810222582b9cb35d3cc942f5de93
liuyubobobo <liuyubobobo@gmail.com>
2017-04-19 13:18:31 -0700

    Chapter 03 section 05 nearly ordered array test cases added for testing basic quicksort implementation.

--- 03-Sorting-Advance/Course Code (C++)/05-Quick-Sort/main.cpp
+++ 03-Sorting-Advance/Course Code (C++)/05-Quick-Sort/main.cpp
@@ -51,6 +51,7 @@ int main() {
 
     int n = 1000000;
 
+    // 测试1 一般性测试
     cout<<"Test for random array, size = "<<n<<", random range [0, "<<n<<"]"<<endl;
     int* arr1 = SortTestHelper::generateRandomArray(n,0,n);
     int* arr2 = SortTestHelper::copyIntArray(arr1,n);
@@ -60,6 +61,24 @@ int main() {
 
     delete[] arr1;
     delete[] arr2;
+
+    cout<<endl;
+
+
+    // 测试2 测试近乎有序的数组
+    // 但是对于近乎有序的数组, 我们的快速排序算法退化成了O(n^2)级别的算法
+    // 思考一下为什么对于近乎有序的数组, 快排退化成了O(n^2)的算法? :)
+//    int swapTimes = 100;
+//    cout<<"Test for nearly ordered array, size = "<<n<<", swap time = "<<swapTimes<<endl;
+//    arr1 = SortTestHelper::generateNearlyOrderedArray(n,swapTimes);
+//    arr2 = SortTestHelper::copyIntArray(arr1, n);
+//
+//    SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
+//    SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
+//
+//    delete[] arr1;
+//    delete[] arr2;
+
 
     return 0;
 }
--- 03-Sorting-Advance/Course Code (Java)/05-Quick-Sort/src/bobo/algo/Main.java
+++ 03-Sorting-Advance/Course Code (Java)/05-Quick-Sort/src/bobo/algo/Main.java
@@ -11,6 +11,7 @@ public class Main {
 
         int N = 1000000;
 
+        // 测试1 一般性测试
         System.out.println("Test for random array, size = " + N + " , random range [0, " + N + "]");
 
         Integer[] arr1 = SortTestHelper.generateRandomArray(N, 0, N);
@@ -20,6 +21,21 @@ public class Main {
         SortTestHelper.testSort("bobo.algo.QuickSort", arr2);
 
         System.out.println();
+
+
+        // 测试2 测试近乎有序的数组
+        // 但是对于近乎有序的数组, 我们的快速排序算法退化成了O(n^2)级别的算法
+        // 思考一下为什么对于近乎有序的数组, 快排退化成了O(n^2)的算法? :)
+        int swapTimes = 100;
+        assert swapTimes >= 0;
+
+        System.out.println("Test for nearly ordered array, size = " + N + " , swap time = " + swapTimes);
+
+        arr1 = SortTestHelper.generateNearlyOrderedArray(N, swapTimes);
+        arr2 = Arrays.copyOf(arr1, arr1.length);
+
+        SortTestHelper.testSort("bobo.algo.MergeSort", arr1);
+        SortTestHelper.testSort("bobo.algo.QuickSort", arr2);
 
         return;
     }
