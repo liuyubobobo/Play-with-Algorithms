d17ab4a0e35e8de043f64a8c2f2e8af28415a9de
liuyubobobo <liuyubobobo@gmail.com>
2017-04-18 18:10:47 -0700

    Chapter 02 Optional C++ code comments updated.

--- 02-Sorting-Basic/Course Code (C++)/Optional-01-Bubble-Sort/SortTestHelper.h
+++ 02-Sorting-Basic/Course Code (C++)/Optional-01-Bubble-Sort/SortTestHelper.h
@@ -16,6 +16,7 @@ using namespace std;
 
 namespace SortTestHelper {
 
+    // 生成有n个元素的随机数组,每个元素的随机范围为[rangeL, rangeR]
     int *generateRandomArray(int n, int range_l, int range_r) {
 
         int *arr = new int[n];
@@ -26,6 +27,9 @@ namespace SortTestHelper {
         return arr;
     }
 
+    // 生成一个近乎有序的数组
+    // 首先生成一个含有[0...n-1]的完全有序数组, 之后随机交换swapTimes对数据
+    // swapTimes定义了数组的无序程度
     int *generateNearlyOrderedArray(int n, int swapTimes){
 
         int *arr = new int[n];
@@ -42,6 +46,7 @@ namespace SortTestHelper {
         return arr;
     }
 
+    // 拷贝整型数组a中的所有元素到一个新的数组, 并返回新的数组
     int *copyIntArray(int a[], int n){
 
         int *arr = new int[n];
@@ -49,6 +54,7 @@ namespace SortTestHelper {
         return arr;
     }
 
+    // 打印arr数组的所有内容
     template<typename T>
     void printArray(T arr[], int n) {
 
@@ -59,6 +65,7 @@ namespace SortTestHelper {
         return;
     }
 
+    // 判断arr数组是否有序
     template<typename T>
     bool isSorted(T arr[], int n) {
 
@@ -69,6 +76,7 @@ namespace SortTestHelper {
         return true;
     }
 
+    // 测试sort排序算法排序arr数组所得到结果的正确性和算法运行时间
     template<typename T>
     void testSort(const string &sortName, void (*sort)(T[], int), T arr[], int n) {
 
