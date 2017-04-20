d42dcf41c077a8caff12f390381bea3939267cd2
liuyubobobo <liuyubobobo@gmail.com>
2017-04-20 14:09:28 -0700

    Chapter 03 section 08 Java code added.

--- 03-Sorting-Advance/Course Code (Java)/08-Quick-Sort-Three-Ways/src/bobo/algo/Main.java
+++ 03-Sorting-Advance/Course Code (Java)/08-Quick-Sort-Three-Ways/src/bobo/algo/Main.java
@@ -1,8 +1,61 @@
 package bobo.algo;
 
+import java.util.Arrays;
+
 public class Main {
 
+    // 比较Merge Sort和双路快速排序和三路快排三种排序算法的性能效率
+    // 对于包含有大量重复数据的数组, 三路快排有巨大的优势
+    // 对于一般性的随机数组和近乎有序的数组, 三路快排的效率虽然不是最优的, 但是是在非常可以接受的范围里
+    // 因此, 在一些语言中, 三路快排是默认的语言库函数中使用的排序算法。比如Java:)
+
     public static void main(String[] args) {
-	// write your code here
+
+        int N = 1000000;
+
+        // 测试1 一般性测试
+        System.out.println("Test for random array, size = " + N + " , random range [0, " + N + "]");
+
+        Integer[] arr1 = SortTestHelper.generateRandomArray(N, 0, N);
+        Integer[] arr2 = Arrays.copyOf(arr1, arr1.length);
+        Integer[] arr3 = Arrays.copyOf(arr1, arr1.length);
+
+        SortTestHelper.testSort("bobo.algo.MergeSort", arr1);
+        SortTestHelper.testSort("bobo.algo.QuickSort2Ways", arr2);
+        SortTestHelper.testSort("bobo.algo.QuickSort3Ways", arr2);
+
+        System.out.println();
+
+
+        // 测试2 测试近乎有序的数组
+        int swapTimes = 100;
+        assert swapTimes >= 0;
+
+        System.out.println("Test for nearly ordered array, size = " + N + " , swap time = " + swapTimes);
+
+        arr1 = SortTestHelper.generateNearlyOrderedArray(N, swapTimes);
+        arr2 = Arrays.copyOf(arr1, arr1.length);
+        arr3 = Arrays.copyOf(arr1, arr1.length);
+
+        SortTestHelper.testSort("bobo.algo.MergeSort", arr1);
+        SortTestHelper.testSort("bobo.algo.QuickSort2Ways", arr2);
+        SortTestHelper.testSort("bobo.algo.QuickSort3Ways", arr2);
+
+        System.out.println();
+
+
+        // 测试3 测试存在包含大量相同元素的数组
+        System.out.println("Test for random array, size = " + N + " , random range [0,10]");
+
+        arr1 = SortTestHelper.generateRandomArray(N, 0, 10);
+        arr2 = Arrays.copyOf(arr1, arr1.length);
+        arr3 = Arrays.copyOf(arr1, arr1.length);
+
+        SortTestHelper.testSort("bobo.algo.MergeSort", arr1);
+        SortTestHelper.testSort("bobo.algo.QuickSort2Ways", arr2);
+        SortTestHelper.testSort("bobo.algo.QuickSort3Ways", arr2);
+
+
+        return;
     }
 }
--- 03-Sorting-Advance/Course Code (Java)/08-Quick-Sort-Three-Ways/src/bobo/algo/QuickSort3Ways.java
+++ 03-Sorting-Advance/Course Code (Java)/08-Quick-Sort-Three-Ways/src/bobo/algo/QuickSort3Ways.java
@@ -16,7 +16,33 @@ public class QuickSort3Ways {
             return;
         }
 
+        // 随机在arr[l...r]的范围中, 选择一个数值作为标定点pivot
+        swap( arr, l, (int)(Math.random()*(r-l+1)) + l );
 
+        Comparable v = arr[l];
+
+        int lt = l;     // arr[l+1...lt] < v
+        int gt = r + 1; // arr[gt...r] > v
+        int i = l+1;    // arr[lt+1...i) == v
+        while( i < gt ){
+            if( arr[i].compareTo(v) < 0 ){
+                swap( arr, i, lt+1);
+                i ++;
+                lt ++;
+            }
+            else if( arr[i].compareTo(v) > 0 ){
+                swap( arr, i, gt-1);
+                gt --;
+            }
+            else{ // arr[i] == v
+                i ++;
+            }
+        }
+
+        swap( arr, l, lt );
+
+        sort(arr, l, lt-1);
+        sort(arr, gt, r);
     }
 
     public static void sort(Comparable[] arr){
