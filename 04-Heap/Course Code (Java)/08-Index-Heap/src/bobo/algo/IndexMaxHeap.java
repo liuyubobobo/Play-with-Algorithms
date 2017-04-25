5a28ac3b980a16216d63bdd362603abf26aa94f6
liuyubobobo <liuyubobobo@gmail.com>
2017-04-24 20:05:37 -0700

    Chapter 04 section 08 Java code added.

--- 04-Heap/Course Code (Java)/08-Index-Heap/src/bobo/algo/IndexMaxHeap.java
+++ 04-Heap/Course Code (Java)/08-Index-Heap/src/bobo/algo/IndexMaxHeap.java
@@ -138,8 +138,41 @@ public class IndexMaxHeap<Item extends Comparable> {
         }
     }
 
+    // 测试索引堆中的索引数组index
+    // 注意:这个测试在向堆中插入元素以后, 不进行extract操作有效
+    public boolean testIndexes(){
+
+        int[] copyIndexes = new int[count+1];
+
+        for( int i = 0 ; i <= count ; i ++ )
+            copyIndexes[i] = indexes[i];
+
+        copyIndexes[0] = 0;
+        Arrays.sort(copyIndexes);
+
+        // 在对索引堆中的索引进行排序后, 应该正好是1...count这count个索引
+        boolean res = true;
+        for( int i = 1 ; i <= count ; i ++ )
+            if( copyIndexes[i-1] + 1 != copyIndexes[i] ){
+                res = false;
+                break;
+            }
+
+        if( !res ){
+            System.out.println("Error!");
+            return false;
+        }
+
+        return true;
+    }
+
     // 测试 IndexMaxHeap
     public static void main(String[] args) {
 
+        int N = 1000000;
+        IndexMaxHeap<Integer> indexMaxHeap = new IndexMaxHeap<Integer>(N);
+        for( int i = 0 ; i < N ; i ++ )
+            indexMaxHeap.insert( i , (int)(Math.random()*N) );
+        assert indexMaxHeap.testIndexes();
     }
 }
