e537d30a14ff4ae1ec6d5d3d270ddc9190fe0df9
liuyubobobo <liuyubobobo@gmail.com>
2017-04-25 20:46:01 -0700

    Chapter 02 Completed Java Code added.

---
+++ 02-Sorting-Basic/Course Code (Java)/Chapter-02-Completed-Code/src/bobo/algo/Main.java
@@ -0,0 +1,48 @@
+package bobo.algo;
+
+import java.util.Arrays;
+
+public class Main {
+
+    // 比较SelectionSort和InsertionSort两种排序算法的性能效率
+    public static void main(String[] args) {
+
+        int N = 20000;
+
+        // 测试1 一般测试
+        System.out.println("Test for random array, size = " + N + " , random range [0, " + N + "]");
+
+        Integer[] arr1 = SortTestHelper.generateRandomArray(N, 0, N);
+        Integer[] arr2 = Arrays.copyOf(arr1, arr1.length);
+
+        SortTestHelper.testSort("bobo.algo.SelectionSort", arr1);
+        SortTestHelper.testSort("bobo.algo.InsertionSort", arr2);
+
+        System.out.println();
+
+
+        // 测试2 有序性更强的测试
+        System.out.println("Test for more ordered random array, size = " + N + " , random range [0,3]");
+
+        arr1 = SortTestHelper.generateRandomArray(N, 0, 3);
+        arr2 = Arrays.copyOf(arr1, arr1.length);
+
+        SortTestHelper.testSort("bobo.algo.SelectionSort", arr1);
+        SortTestHelper.testSort("bobo.algo.InsertionSort", arr2);
+
+        System.out.println();
+
+
+        // 测试3 测试近乎有序的数组
+        int swapTimes = 100;
+        System.out.println("Test for nearly ordered array, size = " + N + " , swap time = " + swapTimes);
+
+        arr1 = SortTestHelper.generateNearlyOrderedArray(N, swapTimes);
+        arr2 = Arrays.copyOf(arr1, arr1.length);
+
+        SortTestHelper.testSort("bobo.algo.SelectionSort", arr1);
+        SortTestHelper.testSort("bobo.algo.InsertionSort", arr2);
+
+        return;
+    }
+}
