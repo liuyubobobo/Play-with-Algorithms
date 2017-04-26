6ade0d5b3ba3224335c6155680795e3dc9f99415
liuyubobobo <liuyubobobo@gmail.com>
2017-04-25 17:20:19 -0700

    Chapter 04 section 09 C++ code and comments updated.

--- 04-Heap/Course Code (C++)/09-Index-Heap-Advance/main.cpp
+++ 04-Heap/Course Code (C++)/09-Index-Heap-Advance/main.cpp
@@ -2,21 +2,21 @@
 #include <cassert>
 #include "SortTestHelper.h"
 
-#define Priority int
-
 using namespace std;
 
+// 最大索引堆
 template<typename Item>
 class IndexMaxHeap{
 
 private:
-    Item *data;
-    int *indexes;
-    int *reverse;
+    Item *data;     // 最大索引堆中的数据
+    int *indexes;   // 最大索引堆中的索引, indexes[x] = i 表示索引i在x的位置
+    int *reverse;   // 最大索引堆中的反向索引, reverse[i] = x 表示索引i在x的位置
 
     int count;
     int capacity;
 
+    // 索引堆中, 数据之间的比较根据data的大小进行比较, 但实际操作的是索引
     void shiftUp( int k ){
 
         while( k > 1 && data[indexes[k/2]] < data[indexes[k]] ){
@@ -27,6 +27,7 @@ private:
         }
     }
 
+    // 索引堆中, 数据之间的比较根据data的大小进行比较, 但实际操作的是索引
     void shiftDown( int k ){
 
         while( 2*k <= count ){
@@ -45,6 +46,7 @@ private:
     }
 
 public:
+    // 构造函数, 构造一个空的索引堆, 可容纳capacity个元素
     IndexMaxHeap(int capacity){
 
         data = new Item[capacity+1];
@@ -63,14 +65,17 @@ public:
         delete[] reverse;
     }
 
+    // 返回索引堆中的元素个数
     int size(){
         return count;
     }
 
+    // 返回一个布尔值, 表示索引堆中是否为空
     bool isEmpty(){
         return count == 0;
     }
 
+    // 向最大索引堆中插入一个新的元素, 新元素的索引为i, 元素为item
     // 传入的i对用户而言,是从0索引的
     void insert(int i, Item item){
         assert( count + 1 <= capacity );
@@ -88,6 +93,7 @@ public:
         shiftUp(count);
     }
 
+    // 从最大索引堆中取出堆顶元素, 即索引堆中所存储的最大数据
     Item extractMax(){
         assert( count > 0 );
 
@@ -100,6 +106,7 @@ public:
         return ret;
     }
 
+    // 从最大索引堆中取出堆顶元素的索引
     int extractMaxIndex(){
         assert( count > 0 );
 
@@ -112,26 +119,31 @@ public:
         return ret;
     }
 
+    // 获取最大索引堆中的堆顶元素
     Item getMax(){
         assert( count > 0 );
         return data[indexes[1]];
     }
 
+    // 获取最大索引堆中的堆顶元素的索引
     int getMaxIndex(){
         assert( count > 0 );
         return indexes[1]-1;
     }
 
+    // 看索引i所在的位置是否存在元素
     bool contain( int i ){
         assert( i + 1 >= 1 && i + 1 <= capacity );
         return reverse[i+1] != 0;
     }
 
+    // 获取最大索引堆中索引为i的元素
     Item getItem( int i ){
         assert( contain(i) );
         return data[i+1];
     }
 
+    // 将最大索引堆中索引为i的元素修改为newItem
     void change( int i , Item newItem ){
 
         assert( contain(i) );
@@ -140,7 +152,6 @@ public:
 
         // 找到indexes[j] = i, j表示data[i]在堆中的位置
         // 之后shiftUp(j), 再shiftDown(j)
-
 //        for( int j = 1 ; j <= count ; j ++ )
 //            if( indexes[j] == i ){
 //                shiftUp(j);
@@ -148,13 +159,16 @@ public:
 //                return;
 //            }
 
+        // 有了 reverse 之后,
+        // 我们可以非常简单的通过reverse直接定位索引i在indexes中的位置
         int j = reverse[i];
         shiftUp( j );
         shiftDown( j );
     }
 
-    // test reverse index
-    bool testReverseIndex(){
+    // 测试索引堆中的索引数组index和反向数组reverse
+    // 注意:这个测试在向堆中插入元素以后, 不进行extract操作有效
+    bool testIndexesAndReverseIndexes(){
 
         int *copyIndexes = new int[count+1];
         int *copyReverseIndexes = new int[count+1];
@@ -168,17 +182,22 @@ public:
         std::sort(copyIndexes, copyIndexes + count + 1);
         std::sort(copyReverseIndexes, copyReverseIndexes + count + 1);
 
+        // 在对索引堆中的索引和反向索引进行排序后,
+        // 两个数组都应该正好是1...count这count个索引
         bool res = true;
         for( int i = 1 ; i <= count ; i ++ )
-            if( copyIndexes[i-1] + 1 != copyIndexes[i] || copyReverseIndexes[i-1] + 1 != copyReverseIndexes[i] )
-                res = res || false;
+            if( copyIndexes[i-1] + 1 != copyIndexes[i] ||
+                    copyReverseIndexes[i-1] + 1 != copyReverseIndexes[i] ){
+                res = false;
+                break;
+            }
 
         delete[] copyIndexes;
         delete[] copyReverseIndexes;
 
         if( !res ){
-            cout<<"Error 1"<<endl;
-            return res;
+            cout<<"Error!"<<endl;
+            return false;
         }
 
         for( int i = 1 ; i <= count ; i ++ )
@@ -191,13 +210,16 @@ public:
     }
 };
 
+// 使用最大索引堆进行堆排序, 来验证我们的最大索引堆的正确性
+// 最大索引堆的主要作用不是用于排序, 我们在这里使用排序只是为了验证我们的最大索引堆实现的正确性
+// 在后续的图论中, 无论是最小生成树算法, 还是最短路径算法, 我们都需要使用索引堆进行优化:)
 template<typename T>
 void heapSortUsingIndexMaxHeap(T arr[], int n){
 
     IndexMaxHeap<T> indexMaxHeap = IndexMaxHeap<T>(n);
     for( int i = 0 ; i < n ; i ++ )
         indexMaxHeap.insert( i , arr[i] );
-    assert( indexMaxHeap.testReverseIndex() );
+    assert( indexMaxHeap.testIndexesAndReverseIndexes() );
 
     for( int i = n-1 ; i >= 0 ; i -- )
         arr[i] = indexMaxHeap.extractMax();
@@ -207,10 +229,7 @@ int main() {
 
     int n = 1000000;
 
-    Priority* arr = new Priority[n];
-    srand(time(NULL));
-    for( int i = 0 ; i < n ; i ++ )
-        arr[i] = rand()%n;
+    int* arr = SortTestHelper::generateRandomArray(n, 0, n);
     SortTestHelper::testSort("Heap Sort Using Index-Max-Heap", heapSortUsingIndexMaxHeap, arr, n);
     delete[] arr;
 
