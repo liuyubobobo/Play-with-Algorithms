6ba402192ad74298812432bdeef2b420b0f33877
liuyubobobo <liuyubobobo@gmail.com>
2017-04-25 23:07:03 -0700

    Chapter 04 Java Chapter Completed Code added.

---
+++ 04-Heap/Course Code (Java)/Chapter-04-Completed-Code/src/bobo/algo/HeapSort.java
@@ -0,0 +1,63 @@
+package bobo.algo;
+
+import java.util.*;
+
+// 不使用一个额外的最大堆, 直接在原数组上进行原地的堆排序
+public class HeapSort {
+
+    // 我们的算法类不允许产生任何实例
+    private HeapSort(){}
+
+    public static void sort(Comparable[] arr){
+
+        int n = arr.length;
+
+        // 注意，此时我们的堆是从0开始索引的
+        // 从(最后一个元素的索引-1)/2开始
+        // 最后一个元素的索引 = n-1
+        for( int i = (n-1-1)/2 ; i >= 0 ; i -- )
+            shiftDown(arr, n, i);
+
+        for( int i = n-1; i > 0 ; i-- ){
+            swap( arr, 0, i);
+            shiftDown(arr, i, 0);
+        }
+    }
+
+    // 交换堆中索引为i和j的两个元素
+    private static void swap(Object[] arr, int i, int j){
+        Object t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
+    }
+
+    // 优化的shiftDown过程, 使用赋值的方式取代不断的swap,
+    // 该优化思想和我们之前对插入排序进行优化的思路是一致的
+    private static void shiftDown(Comparable[] arr, int n, int k){
+
+        Comparable e = arr[k];
+        while( 2*k+1 < n ){
+            int j = 2*k+1;
+            if( j+1 < n && arr[j+1].compareTo(arr[j]) > 0 )
+                j += 1;
+
+            if( e.compareTo(arr[j]) >= 0 )
+                break;
+
+            arr[k] = arr[j];
+            k = j;
+        }
+
+        arr[k] = e;
+    }
+
+    // 测试 HeapSort
+    public static void main(String[] args) {
+
+        int N = 1000000;
+        Integer[] arr = SortTestHelper.generateRandomArray(N, 0, 100000);
+        SortTestHelper.testSort("bobo.algo.HeapSort", arr);
+
+        return;
+    }
+}
---
+++ 04-Heap/Course Code (Java)/Chapter-04-Completed-Code/src/bobo/algo/MaxHeap.java
@@ -0,0 +1,129 @@
+package bobo.algo;
+
+import java.util.*;
+import java.lang.*;
+
+// 在堆的有关操作中，需要比较堆中元素的大小，所以Item需要extends Comparable
+public class MaxHeap<Item extends Comparable> {
+
+    protected Item[] data;
+    protected int count;
+    protected int capacity;
+
+    // 构造函数, 构造一个空堆, 可容纳capacity个元素
+    public MaxHeap(int capacity){
+        data = (Item[])new Comparable[capacity+1];
+        count = 0;
+        this.capacity = capacity;
+    }
+
+    // 构造函数, 通过一个给定数组创建一个最大堆
+    // 该构造堆的过程, 时间复杂度为O(n)
+    public MaxHeap(Item arr[]){
+
+        int n = arr.length;
+
+        data = (Item[])new Comparable[n+1];
+        capacity = n;
+
+        for( int i = 0 ; i < n ; i ++ )
+            data[i+1] = arr[i];
+        count = n;
+
+        for( int i = count/2 ; i >= 1 ; i -- )
+            shiftDown(i);
+    }
+
+    // 返回堆中的元素个数
+    public int size(){
+        return count;
+    }
+
+    // 返回一个布尔值, 表示堆中是否为空
+    public boolean isEmpty(){
+        return count == 0;
+    }
+
+    // 像最大堆中插入一个新的元素 item
+    public void insert(Item item){
+
+        assert count + 1 <= capacity;
+        data[count+1] = item;
+        count ++;
+        shiftUp(count);
+    }
+
+    // 从最大堆中取出堆顶元素, 即堆中所存储的最大数据
+    public Item extractMax(){
+        assert count > 0;
+        Item ret = data[1];
+
+        swap( 1 , count );
+        count --;
+        shiftDown(1);
+
+        return ret;
+    }
+
+    // 获取最大堆中的堆顶元素
+    public Item getMax(){
+        assert( count > 0 );
+        return data[1];
+    }
+
+
+    // 交换堆中索引为i和j的两个元素
+    private void swap(int i, int j){
+        Item t = data[i];
+        data[i] = data[j];
+        data[j] = t;
+    }
+
+    //********************
+    //* 最大堆核心辅助函数
+    //********************
+    private void shiftUp(int k){
+
+        while( k > 1 && data[k/2].compareTo(data[k]) < 0 ){
+            swap(k, k/2);
+            k /= 2;
+        }
+    }
+
+    private void shiftDown(int k){
+
+        while( 2*k <= count ){
+            int j = 2*k; // 在此轮循环中,data[k]和data[j]交换位置
+            if( j+1 <= count && data[j+1].compareTo(data[j]) > 0 )
+                j ++;
+            // data[j] 是 data[2*k]和data[2*k+1]中的最大值
+
+            if( data[k].compareTo(data[j]) >= 0 ) break;
+            swap(k, j);
+            k = j;
+        }
+    }
+
+    // 测试 MaxHeap
+    public static void main(String[] args) {
+
+        MaxHeap<Integer> maxHeap = new MaxHeap<Integer>(100);
+        int N = 100; // 堆中元素个数
+        int M = 100; // 堆中元素取值范围[0, M)
+        for( int i = 0 ; i < N ; i ++ )
+            maxHeap.insert( new Integer((int)(Math.random() * M)) );
+
+        Integer[] arr = new Integer[N];
+        // 将maxheap中的数据逐渐使用extractMax取出来
+        // 取出来的顺序应该是按照从大到小的顺序取出来的
+        for( int i = 0 ; i < N ; i ++ ){
+            arr[i] = maxHeap.extractMax();
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        // 确保arr数组是从大到小排列的
+        for( int i = 1 ; i < N ; i ++ )
+            assert arr[i-1] >= arr[i];
+    }
+}
