8d15a1d24847097bcf4ac7fec3edecd26aca9dd1
liuyubobobo <liuyubobobo@gmail.com>
2017-05-03 10:24:22 -0700

    Chapter 05 section 02-03 C++ code updated.

--- 05-Binary-Search-Tree/Course Code (C++)/02-Binary-Search-Tree-Basics/main.cpp
+++ 05-Binary-Search-Tree/Course Code (C++)/02-Binary-Search-Tree-Basics/main.cpp
@@ -2,11 +2,12 @@
 
 using namespace std;
 
+// 二分搜索树
 template <typename Key, typename Value>
 class BST{
 
 private:
-    // 树中的节点为私有的结构体, 外界不需要了解二叉树节点的具体实现
+    // 二分搜索树中的节点为私有的结构体, 外界不需要了解二分搜索树节点的具体实现
     struct Node{
         Key key;
         Value value;
@@ -21,10 +22,10 @@ private:
     };
 
     Node *root; // 根节点
-    int count;  // 树中的节点个数
+    int count;  // 节点个数
 
 public:
-    // 构造函数, 默认构造一棵空二叉树
+    // 构造函数, 默认构造一棵空二分搜索树
     BST(){
         root = NULL;
         count = 0;
@@ -33,12 +34,12 @@ public:
         // TODO: ~BST()
     }
 
-    // 返回二叉树的节点个数
+    // 返回二分搜索树的节点个数
     int size(){
         return count;
     }
 
-    // 返回二叉树是否为空
+    // 返回二分搜索树是否为空
     bool isEmpty(){
         return count == 0;
     }
--- 05-Binary-Search-Tree/Course Code (C++)/03-Binary-Search-Tree-Insert/main.cpp
+++ 05-Binary-Search-Tree/Course Code (C++)/03-Binary-Search-Tree-Insert/main.cpp
@@ -2,11 +2,12 @@
 
 using namespace std;
 
+// 二分搜索树
 template <typename Key, typename Value>
 class BST{
 
 private:
-    // 树中的节点为私有的结构体, 外界不需要了解二叉树节点的具体实现
+    // 树中的节点为私有的结构体, 外界不需要了解二分搜索树节点的具体实现
     struct Node{
         Key key;
         Value value;
@@ -24,7 +25,7 @@ private:
     int count;  // 树中的节点个数
 
 public:
-    // 构造函数, 默认构造一棵空二叉树
+    // 构造函数, 默认构造一棵空二分搜索树
     BST(){
         root = NULL;
         count = 0;
@@ -33,24 +34,24 @@ public:
         // TODO: ~BST()
     }
 
-    // 返回二叉树的节点个数
+    // 返回二分搜索树的节点个数
     int size(){
         return count;
     }
 
-    // 返回二叉树是否为空
+    // 返回二分搜索树是否为空
     bool isEmpty(){
         return count == 0;
     }
 
-    // 向二叉树中插入一个新的(key, value)数据对
+    // 向二分搜索树中插入一个新的(key, value)数据对
     void insert(Key key, Value value){
         root = insert(root, key, value);
     }
 
 private:
-    // 向以node为根的二叉搜索树中, 插入节点(key, value), 使用递归算法
-    // 返回插入新节点后的二叉搜索树的根
+    // 向以node为根的二分搜索树中, 插入节点(key, value), 使用递归算法
+    // 返回插入新节点后的二分搜索树的根
     Node* insert(Node *node, Key key, Value value){
 
         if( node == NULL ){
