4b2ff843647fac548295384cded90d29ab80d420
liuyubobobo <liuyubobobo@gmail.com>
2017-05-04 17:35:12 -0700

    Chapter 5 Optional 02 C++ code comments updated.

--- 05-Binary-Search-Tree/Course Code (C++)/Optional-02-Floor-and-Ceil/main.cpp
+++ 05-Binary-Search-Tree/Course Code (C++)/Optional-02-Floor-and-Ceil/main.cpp
@@ -4,10 +4,12 @@
 
 using namespace std;
 
+// 二分搜索树
 template <typename Key, typename Value>
 class BST{
 
 private:
+    // 树中的节点为私有的结构体, 外界不需要了解二分搜索树节点的具体实现
     struct Node{
         Key key;
         Value value;
@@ -28,54 +30,62 @@ private:
         }
     };
 
-    Node *root;
-    int count;
+    Node *root; // 根节点
+    int count;  // 树中的节点个数
 
 public:
+    // 构造函数, 默认构造一棵空二分搜索树
     BST(){
         root = NULL;
         count = 0;
     }
+
+    // 析构函数, 释放二分搜索树的所有空间
     ~BST(){
         destroy( root );
     }
 
+    // 返回二分搜索树的节点个数
     int size(){
         return count;
     }
 
+    // 返回二分搜索树是否为空
     bool isEmpty(){
         return count == 0;
     }
 
+    // 向二分搜索树中插入一个新的(key, value)数据对
     void insert(Key key, Value value){
         root = insert(root, key, value);
     }
 
+    // 查看二分搜索树中是否存在键key
     bool contain(Key key){
         return contain(root, key);
     }
 
+    // 在二分搜索树中搜索键key所对应的值。如果这个值不存在, 则返回NULL
     Value* search(Key key){
         return search( root , key );
     }
 
-    // 前序遍历
+    // 二分搜索树的前序遍历
     void preOrder(){
         preOrder(root);
     }
 
-    // 中序遍历
+    // 二分搜索树的中序遍历
     void inOrder(){
         inOrder(root);
     }
 
-    // 后序遍历
+    // 二分搜索树的后序遍历
     void postOrder(){
         postOrder(root);
     }
 
-    // 层序遍历
+    // 二分搜索树的层序遍历
     void levelOrder(){
 
         queue<Node*> q;
@@ -94,21 +104,38 @@ public:
         }
     }
 
-    // 寻找最小的键值
+    // 寻找二分搜索树的最小的键值
     Key minimum(){
         assert( count != 0 );
         Node* minNode = minimum( root );
         return minNode->key;
     }
 
-    // 寻找最大的键值
+    // 寻找二分搜索树的最大的键值
     Key maximum(){
         assert( count != 0 );
         Node* maxNode = maximum(root);
         return maxNode->key;
     }
 
-    // 寻找key的floor值
+    // 从二分搜索树中删除最小值所在节点
+    void removeMin(){
+        if( root )
+            root = removeMin( root );
+    }
+
+    // 从二分搜索树中删除最大值所在节点
+    void removeMax(){
+        if( root )
+            root = removeMax( root );
+    }
+
+    // 从二分搜索树中删除键值为key的节点
+    void remove(Key key){
+        root = remove(root, key);
+    }
+
+    // 寻找key的floor值, 递归算法
     // 如果不存在key的floor值(key比BST中的最小值还小), 返回NULL
     Key* floor(Key key){
 
@@ -119,7 +146,7 @@ public:
         return &(floorNode->key);
     }
 
-    // 寻找key的ceil值
+    // 寻找key的ceil值, 递归算法
     // 如果不存在key的ceil值(key比BST中的最大值还大), 返回NULL
     Key* ceil(Key key){
 
@@ -130,26 +157,9 @@ public:
         return &(ceilNode->key);
     }
 
-    // 从二叉树中删除最小值所在节点
-    void removeMin(){
-        if( root )
-            root = removeMin( root );
-    }
-
-    // 从二叉树中删除最大值所在节点
-    void removeMax(){
-        if( root )
-            root = removeMax( root );
-    }
-
-    // 从二叉树中删除键值为key的节点
-    void remove(Key key){
-        root = remove(root, key);
-    }
-
 private:
-    // 向以node为根的二叉搜索树中,插入节点(key, value)
-    // 返回插入新节点后的二叉搜索树的根
+    // 向以node为根的二分搜索树中, 插入节点(key, value), 使用递归算法
+    // 返回插入新节点后的二分搜索树的根
     Node* insert(Node *node, Key key, Value value){
 
         if( node == NULL ){
@@ -167,7 +177,7 @@ private:
         return node;
     }
 
-    // 查看以node为根的二叉搜索树中是否包含键值为key的节点
+    // 查看以node为根的二分搜索树中是否包含键值为key的节点, 使用递归算法
     bool contain(Node* node, Key key){
 
         if( node == NULL )
@@ -181,7 +191,8 @@ private:
             return contain( node->right , key );
     }
 
-    // 在以node为根的二叉搜索树中查找key所对应的value
+    // 在以node为根的二分搜索树中查找key所对应的value, 递归算法
+    // 若value不存在, 则返回NULL
     Value* search(Node* node, Key key){
 
         if( node == NULL )
@@ -195,7 +206,7 @@ private:
             return search( node->right, key );
     }
 
-    // 对以node为根的二叉搜索树进行前序遍历
+    // 对以node为根的二分搜索树进行前序遍历, 递归算法
     void preOrder(Node* node){
 
         if( node != NULL ){
@@ -205,7 +216,7 @@ private:
         }
     }
 
-    // 对以node为根的二叉搜索树进行中序遍历
+    // 对以node为根的二分搜索树进行中序遍历, 递归算法
     void inOrder(Node* node){
 
         if( node != NULL ){
@@ -215,7 +226,7 @@ private:
         }
     }
 
-    // 对以node为根的二叉搜索树进行后序遍历
+    // 对以node为根的二分搜索树进行后序遍历, 递归算法
     void postOrder(Node* node){
 
         if( node != NULL ){
@@ -225,6 +236,8 @@ private:
         }
     }
 
+    // 释放以node为根的二分搜索树的所有节点
+    // 采用后续遍历的递归算法
     void destroy(Node* node){
 
         if( node != NULL ){
@@ -236,7 +249,7 @@ private:
         }
     }
 
-    // 在以node为根的二叉搜索树中,返回最小键值的节点
+    // 返回以node为根的二分搜索树的最小键值所在的节点, 递归算法
     Node* minimum(Node* node){
         if( node->left == NULL )
             return node;
@@ -244,7 +257,7 @@ private:
         return minimum(node->left);
     }
 
-    // 在以node为根的二叉搜索树中,返回最大键值的节点
+    // 返回以node为根的二分搜索树的最大键值所在的节点, 递归算法
     Node* maximum(Node* node){
         if( node->right == NULL )
             return node;
@@ -252,7 +265,7 @@ private:
         return maximum(node->right);
     }
 
-    // 删除掉以node为根的二分搜索树中的最小节点
+    // 删除掉以node为根的二分搜索树中的最小节点, 递归算法
     // 返回删除节点后新的二分搜索树的根
     Node* removeMin(Node* node){
 
@@ -268,7 +281,7 @@ private:
         return node;
     }
 
-    // 删除掉以node为根的二分搜索树中的最大节点
+    // 删除掉以node为根的二分搜索树中的最大节点, 递归算法
     // 返回删除节点后新的二分搜索树的根
     Node* removeMax(Node* node){
 
@@ -284,7 +297,7 @@ private:
         return node;
     }
 
-    // 删除掉以node为根的二分搜索树中键值为key的节点
+    // 删除掉以node为根的二分搜索树中键值为key的节点, 递归算法
     // 返回删除节点后新的二分搜索树的根
     Node* remove(Node* node, Key key){
 
@@ -329,8 +342,7 @@ private:
         }
     }
 
-
-    // 在以node为根的二叉搜索树中, 寻找key的floor值所处的节点
+    // 在以node为根的二叉搜索树中, 寻找key的floor值所处的节点, 递归算法
     Node* floor(Node* node, Key key){
 
         if( node == NULL )
@@ -357,7 +369,7 @@ private:
     }
 
 
-    // 在以node为根的二叉搜索树中, 寻找key的ceil值所处的节点
+    // 在以node为根的二叉搜索树中, 寻找key的ceil值所处的节点, 递归算法
     Node* ceil(Node* node, Key key){
 
         if( node == NULL )
@@ -385,12 +397,16 @@ private:
 };
 
 
+// 测试二分搜索树中的floor和ceil两个函数
 int main(){
 
     BST<int,int> bst;
 
+    // 向二分搜索树中插入从2到20(不包括20)d的所有偶数
     for(int i = 2 ; i < 20 ; i += 2 )
         bst.insert(i, i);
+
+    // 对[0...20]区间里的21个数, 调用二分搜索树的floor和ceil, 查看其结果
     for( int i = 0 ; i <= 20 ; i ++ ){
 
         int* floorKey = bst.floor(i);
