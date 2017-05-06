ab3af89601e2f46b72cd507ec41704bfebe98c82
liuyubobobo <liuyubobobo@gmail.com>
2017-05-05 22:06:15 -0700

    Chapter 06 Java Completed Code added.

---
+++ 06-Union-Find/Course Code (Java)/Chapter-06-Completed-Code/src/bobo/algo/UnionFindTestHelper.java
@@ -0,0 +1,135 @@
+package bobo.algo;
+
+// 测试并查集
+public class UnionFindTestHelper {
+
+    // 我们的测试类不允许创建实例
+    private UnionFindTestHelper(){}
+
+    // 测试第一版本的并查集, 测试元素个数为n
+    public static void testUF1( int n ){
+
+        UnionFind1 uf = new UnionFind1(n);
+
+        long startTime = System.currentTimeMillis();
+
+        // 进行n次操作, 每次随机选择两个元素进行合并操作
+        for( int i = 0 ; i < n ; i ++ ){
+            int a = (int)(Math.random()*n);
+            int b = (int)(Math.random()*n);
+            uf.unionElements(a,b);
+        }
+        // 再进行n次操作, 每次随机选择两个元素, 查询他们是否同属一个集合
+        for(int i = 0 ; i < n ; i ++ ){
+            int a = (int)(Math.random()*n);
+            int b = (int)(Math.random()*n);
+            uf.isConnected(a,b);
+        }
+        long endTime = System.currentTimeMillis();
+
+        // 打印输出对这2n个操作的耗时
+        System.out.println("UF1, " + 2*n + " ops, " + (endTime-startTime) + "ms");
+    }
+
+    // 测试第二版本的并查集, 测试元素个数为n, 测试逻辑和之前是完全一样的
+    // 思考一下: 这样的冗余代码如何消除?
+    // 由于这个课程不是设计模式课程, 在这里就不过多引入相关的问题讲解了。留作给大家的思考题:)
+    public static void testUF2( int n ){
+
+        UnionFind2 uf = new UnionFind2(n);
+
+        long startTime = System.currentTimeMillis();
+
+        // 进行n次操作, 每次随机选择两个元素进行合并操作
+        for( int i = 0 ; i < n ; i ++ ){
+            int a = (int)(Math.random()*n);
+            int b = (int)(Math.random()*n);
+            uf.unionElements(a,b);
+        }
+        // 再进行n次操作, 每次随机选择两个元素, 查询他们是否同属一个集合
+        for(int i = 0 ; i < n ; i ++ ){
+            int a = (int)(Math.random()*n);
+            int b = (int)(Math.random()*n);
+            uf.isConnected(a,b);
+        }
+        long endTime = System.currentTimeMillis();
+
+        // 打印输出对这2n个操作的耗时
+        System.out.println("UF2, " + 2*n + " ops, " + (endTime-startTime) + "ms");
+    }
+
+    // 测试第三版本的并查集, 测试元素个数为n, 测试逻辑和之前是完全一样的
+    public static void testUF3( int n ){
+
+        UnionFind3 uf = new UnionFind3(n);
+
+        long startTime = System.currentTimeMillis();
+
+        // 进行n次操作, 每次随机选择两个元素进行合并操作
+        for( int i = 0 ; i < n ; i ++ ){
+            int a = (int)(Math.random()*n);
+            int b = (int)(Math.random()*n);
+            uf.unionElements(a,b);
+        }
+        // 再进行n次操作, 每次随机选择两个元素, 查询他们是否同属一个集合
+        for(int i = 0 ; i < n ; i ++ ){
+            int a = (int)(Math.random()*n);
+            int b = (int)(Math.random()*n);
+            uf.isConnected(a,b);
+        }
+        long endTime = System.currentTimeMillis();
+
+        // 打印输出对这2n个操作的耗时
+        System.out.println("UF3, " + 2*n + " ops, " + (endTime-startTime) + "ms");
+    }
+
+    // 测试第四版本的并查集, 测试元素个数为n, 测试逻辑和之前是完全一样的
+    public static void testUF4( int n ){
+
+        UnionFind4 uf = new UnionFind4(n);
+
+        long startTime = System.currentTimeMillis();
+
+        // 进行n次操作, 每次随机选择两个元素进行合并操作
+        for( int i = 0 ; i < n ; i ++ ){
+            int a = (int)(Math.random()*n);
+            int b = (int)(Math.random()*n);
+            uf.unionElements(a,b);
+        }
+        // 再进行n次操作, 每次随机选择两个元素, 查询他们是否同属一个集合
+        for(int i = 0 ; i < n ; i ++ ){
+            int a = (int)(Math.random()*n);
+            int b = (int)(Math.random()*n);
+            uf.isConnected(a,b);
+        }
+        long endTime = System.currentTimeMillis();
+
+        // 打印输出对这2n个操作的耗时
+        System.out.println("UF4, " + 2*n + " ops, " + (endTime-startTime) + "ms");
+    }
+
+    // 测试第五版本的并查集, 测试元素个数为n, 测试逻辑和之前是完全一样的
+    public static void testUF5( int n ){
+
+        UnionFind5 uf = new UnionFind5(n);
+
+        long startTime = System.currentTimeMillis();
+
+        // 进行n次操作, 每次随机选择两个元素进行合并操作
+        for( int i = 0 ; i < n ; i ++ ){
+            int a = (int)(Math.random()*n);
+            int b = (int)(Math.random()*n);
+            uf.unionElements(a,b);
+        }
+        // 再进行n次操作, 每次随机选择两个元素, 查询他们是否同属一个集合
+        for(int i = 0 ; i < n ; i ++ ){
+            int a = (int)(Math.random()*n);
+            int b = (int)(Math.random()*n);
+            uf.isConnected(a,b);
+        }
+        long endTime = System.currentTimeMillis();
+
+        // 打印输出对这2n个操作的耗时
+        System.out.println("UF4, " + 2*n + " ops, " + (endTime-startTime) + "ms");
+    }
+}
