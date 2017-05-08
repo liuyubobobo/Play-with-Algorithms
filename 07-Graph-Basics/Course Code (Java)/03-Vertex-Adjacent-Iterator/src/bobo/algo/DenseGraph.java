766b0bdb20f06a1368f7e2a18199917857e1178b
liuyubobobo <liuyubobobo@gmail.com>
2017-05-07 17:49:00 -0700

    Chapter 07 section 03 C++ code comments updated. Java code added.

---
+++ 07-Graph-Basics/Course Code (Java)/03-Vertex-Adjacent-Iterator/src/bobo/algo/DenseGraph.java
@@ -0,0 +1,86 @@
+package bobo.algo;
+
+// 稠密图 - 邻接矩阵
+public class DenseGraph {
+
+    private int n;  // 节点数
+    private int m;  // 边数
+    private boolean directed;   // 是否为有向图
+    private boolean[][] g;      // 图的具体数据
+
+    // 构造函数
+    public DenseGraph( int n , boolean directed ){
+        assert n >= 0;
+        this.n = n;
+        this.m = 0;    // 初始化没有任何边
+        this.directed = directed;
+        // g初始化为n*n的布尔矩阵, 每一个g[i][j]均为false, 表示没有任和边
+        // false为boolean型变量的默认值
+        g = new boolean[n][n];
+    }
+
+    public int V(){ return n;} // 返回节点个数
+    public int E(){ return m;} // 返回边的个数
+
+    // 向图中添加一个边
+    public void addEdge( int v , int w ){
+
+        assert v >= 0 && v < n ;
+        assert w >= 0 && w < n ;
+
+        if( hasEdge( v , w ) )
+            return;
+
+        g[v][w] = true;
+        if( !directed )
+            g[w][v] = true;
+
+        m ++;
+    }
+
+    // 验证图中是否有从v到w的边
+    boolean hasEdge( int v , int w ){
+        assert v >= 0 && v < n ;
+        assert w >= 0 && w < n ;
+        return g[v][w];
+    }
+
+    // 邻边迭代器, 传入一个图和一个顶点,
+    // 迭代在这个图中和这个顶点向连的所有顶点
+    class adjIterator{
+        private int v;
+        private int index;
+
+        // 构造函数
+        public adjIterator(int v){
+            assert v >= 0 && v < n;
+            this.v = v;
+            this.index = -1;   // 索引从-1开始, 因为每次遍历都需要调用一次next()
+        }
+
+        // 返回图G中与顶点v相连接的第一个顶点
+        public int begin(){
+
+            // 索引从-1开始, 因为每次遍历都需要调用一次next()
+            index = -1;
+            return next();
+        }
+
+        // 返回图G中与顶点v相连接的下一个顶点
+        public int next(){
+
+            // 从当前index开始向后搜索, 直到找到一个g[v][index]为true
+            for( index += 1 ; index < V() ; index ++ )
+                if( g[v][index] )
+                    return index;
+            // 若没有顶点和v相连接, 则返回-1
+            return -1;
+        }
+
+        // 查看是否已经迭代完了图G中与顶点v相连接的所有顶点
+        public boolean end(){
+            return index >= V();
+        }
+
+    };
+}
