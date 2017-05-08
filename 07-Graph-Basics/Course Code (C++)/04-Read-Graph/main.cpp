8a633796a29e0daa11f1922024519abd63fa0c8f
liuyubobobo <liuyubobobo@gmail.com>
2017-05-07 18:44:51 -0700

    Chapter 07 section 04 C++ code comments updated. Java code added.

--- 07-Graph-Basics/Course Code (C++)/04-Read-Graph/main.cpp
+++ 07-Graph-Basics/Course Code (C++)/04-Read-Graph/main.cpp
@@ -5,20 +5,38 @@
 
 using namespace std;
 
-
+// 测试通过文件读取图的信息
 int main() {
 
+    // 使用两种图的存储方式读取testG1.txt文件
     string filename = "testG1.txt";
-
     SparseGraph g1( 13 , false );
     ReadGraph<SparseGraph> readGraph1( g1 , filename );
+    cout<<"test G1 in Sparse Graph:" << endl;
     g1.show();
 
     cout<<endl;
 
     DenseGraph g2( 13 , false );
     ReadGraph<DenseGraph> readGraph2( g2 , filename );
+    cout<<"test G1 in Dense Graph:" << endl;
     g2.show();
+
+    cout<<endl;
+
+    // 使用两种图的存储方式读取testG2.txt文件
+    filename = "testG2.txt";
+    SparseGraph g3( 6 , false );
+    ReadGraph<SparseGraph> readGraph3( g3 , filename );
+    cout<<"test G2 in Sparse Graph:" << endl;
+    g3.show();
+
+    cout<<endl;
+
+    DenseGraph g4( 6 , false );
+    ReadGraph<DenseGraph> readGraph4( g4 , filename );
+    cout<<"test G2 in Dense Graph:" << endl;
+    g4.show();
 
     return 0;
 }
---
+++ 07-Graph-Basics/Course Code (Java)/04-Read-Graph/src/bobo/algo/Main.java
@@ -0,0 +1,38 @@
+package bobo.algo;
+
+// 测试通过文件读取图的信息
+public class Main {
+
+    public static void main(String[] args) {
+
+        // 使用两种图的存储方式读取testG1.txt文件
+        String filename = "testG1.txt";
+        SparseGraph g1 = new SparseGraph(13, false);
+        ReadGraph readGraph1 = new ReadGraph(g1, filename);
+        System.out.println("test G1 in Sparse Graph:");
+        g1.show();
+
+        System.out.println();
+
+        DenseGraph g2 = new DenseGraph(13, false);
+        ReadGraph readGraph2 = new ReadGraph(g2 , filename );
+        System.out.println("test G1 in Dense Graph:");
+        g2.show();
+
+        System.out.println();
+
+        // 使用两种图的存储方式读取testG2.txt文件
+        filename = "testG2.txt";
+        SparseGraph g3 = new SparseGraph(6, false);
+        ReadGraph readGraph3 = new ReadGraph(g3, filename);
+        System.out.println("test G2 in Sparse Graph:");
+        g3.show();
+
+        System.out.println();
+
+        DenseGraph g4 = new DenseGraph(6, false);
+        ReadGraph readGraph4 = new ReadGraph(g4, filename);
+        System.out.println("test G2 in Dense Graph:");
+        g4.show();
+    }
+}
