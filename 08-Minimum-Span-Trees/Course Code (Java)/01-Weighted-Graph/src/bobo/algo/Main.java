7d6fd14dc0c9ff446e4282e21afee3c22386808a
liuyubobobo <liuyubobobo@gmail.com>
2017-05-08 16:38:59 -0700

    Chapter 08 section 01 Java code added.

---
+++ 08-Minimum-Span-Trees/Course Code (Java)/01-Weighted-Graph/src/bobo/algo/Main.java
@@ -0,0 +1,24 @@
+package bobo.algo;
+
+public class Main {
+
+    // 测试通过文件读取图的信息
+    public static void main(String[] args) {
+
+        // 使用两种图的存储方式读取testG1.txt文件
+        String filename = "testG1.txt";
+        SparseWeightedGraph<Double> g1 = new SparseWeightedGraph<Double>(8, false);
+        ReadWeightedGraph readGraph1 = new ReadWeightedGraph(g1, filename);
+        System.out.println("test G1 in Sparse Weighted Graph:");
+        g1.show();
+
+        System.out.println();
+
+        DenseWeightedGraph<Double> g2 = new DenseWeightedGraph<Double>(8, false);
+        ReadWeightedGraph readGraph2 = new ReadWeightedGraph(g2 , filename );
+        System.out.println("test G1 in Dense Graph:");
+        g2.show();
+
+        System.out.println();
+    }
+}
