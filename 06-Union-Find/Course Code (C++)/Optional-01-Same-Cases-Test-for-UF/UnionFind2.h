46bae3a2224c759f525ac2e06a042009d384111e
liuyubobobo <liuyubobobo@gmail.com>
2017-05-16 10:31:45 -0700

    Chapter 06 added a new optional case. To test our UFs performance, we try to use the same test cases:)

---
+++ 06-Union-Find/Course Code (C++)/Optional-01-Same-Cases-Test-for-UF/UnionFind2.h
@@ -0,0 +1,69 @@
+//
+// Created by liuyubobobo on 5/5/17.
+//
+
+#ifndef INC_06_PATH_COMPRESSION_UNIONFIND2_H
+#define INC_06_PATH_COMPRESSION_UNIONFIND2_H
+
+#include <cassert>
+
+using namespace std;
+
+// 我们的第二版Union-Find
+namespace UF2{
+
+    class UnionFind{
+
+    private:
+        // 我们的第二版Union-Find, 使用一个数组构建一棵指向父节点的树
+        // parent[i]表示第一个元素所指向的父节点
+        int* parent;
+        int count;  // 数据个数
+
+    public:
+        // 构造函数
+        UnionFind(int count){
+            parent = new int[count];
+            this->count = count;
+            // 初始化, 每一个parent[i]指向自己, 表示每一个元素自己自成一个集合
+            for( int i = 0 ; i < count ; i ++ )
+                parent[i] = i;
+        }
+
+        // 析构函数
+        ~UnionFind(){
+            delete[] parent;
+        }
+
+        // 查找过程, 查找元素p所对应的集合编号
+        int find(int p){
+            assert( p >= 0 && p < count );
+            // 不断去查询自己的父亲节点, 直到到达根节点
+            // 根节点的特点: parent[p] == p
+            while( p != parent[p] )
+                p = parent[p];
+            return p;
+        }
+
+        // 查看元素p和元素q是否所属一个集合
+        // O(h)复杂度, h为树的高度
+        bool isConnected( int p , int q ){
+            return find(p) == find(q);
+        }
+
+        // 合并元素p和元素q所属的集合
+        // O(h)复杂度, h为树的高度
+        void unionElements(int p, int q){
+
+            int pRoot = find(p);
+            int qRoot = find(q);
+
+            if( pRoot == qRoot )
+                return;
+
+            parent[pRoot] = qRoot;
+        }
+    };
+}
+
+#endif //INC_06_PATH_COMPRESSION_UNIONFIND2_H
---
+++ 06-Union-Find/Course Code (C++)/Optional-01-Same-Cases-Test-for-UF/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include "UnionFindTestHelper.h"
+
+using namespace std;
+
+// 对比UF1, UF2, UF3, UF4和UF5的时间性能
+// 在这里, 我们对于不同的UnionFind的实现, 使用相同的测试用例, 让测试结果更加准确
+int main() {
+
+    // 使用1000000的数据规模
+    int n = 1000000;
+
+    srand( time(NULL) );
+
+    // 生成unionElements的测试用例
+    vector<pair<int,int>> unionTest;
+    for(int i = 0 ; i < n ; i ++){
+        int a = rand()%n;
+        int b = rand()%n;
+        unionTest.push_back(make_pair(a,b));
+    }
+
+    // 生成isConnected的测试用例
+    vector<pair<int,int>> connectTest;
+    for(int i = 0 ; i < n ; i ++){
+        int a = rand()%n;
+        int b = rand()%n;
+        connectTest.push_back(make_pair(a,b));
+    }
+
+    // 测试我们的UF1 ~ UF5
+
+    // 100万数据对于UF1和UF2来说太慢了, 不再测试
+//    UF1::UnionFind uf1 = UF1::UnionFind(n);
+//    UnionFindTestHelper::testUF("UF1", uf1, unionTest, connectTest);
+//
+//    UF2::UnionFind uf2 = UF2::UnionFind(n);
+//    UnionFindTestHelper::testUF("UF2", uf2, unionTest, connectTest);
+
+    UF3::UnionFind uf3 = UF3::UnionFind(n);
+    UnionFindTestHelper::testUF("UF3", uf3, unionTest, connectTest);
+
+    UF4::UnionFind uf4 = UF4::UnionFind(n);
+    UnionFindTestHelper::testUF("UF4", uf4, unionTest, connectTest);
+
+    UF5::UnionFind uf5 = UF5::UnionFind(n);
+    UnionFindTestHelper::testUF("UF5", uf5, unionTest, connectTest);
+
+    return 0;
+}
